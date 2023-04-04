8d398e1c8afc1270a5f8896ee205d104fb747498
arya <64514807+arya3d@users.noreply.github.com>
2023-04-04 16:25:56 +0330

    Update macros.glsl

--- functions/glsl/macros.glsl
+++ functions/glsl/macros.glsl
@@ -1,5 +1,46 @@
 // COLOR_RAMP ------------------------------------------------
 
+Version 1 ( oldest version, checkout the newer versions down below ):
+
+/* ** ColorStop Point data structure for the COLOR_RAMP macro 
+color -> the color of the color stop
+position -> position of the color stop on the color ramp -> [0, 1]
+*/
+struct ColorStop {
+    vec3 color;
+    float position;
+};
+
+/* ** COLOR_RAMP macro -> based on Blender's ColorRamp Node in the shading tab
+ColorStop[?] colors -> array of color stops that can have any length
+float factor -> the position that you want to know the color of -> [0, 1]
+vec3 finalColor -> the final color based on the factor 
+
+Line 5 Of The Macro:
+// possibly is bad for performance 
+index = isInBetween ? i : index; \
+
+Taken From: https://stackoverflow.com/a/26219603/19561482 
+index = int(mix(float(index), float(i), float(isInBetween))); \
+*/
+#define COLOR_RAMP(colors, factor, finalColor) { \
+    int index = 0; \
+    for(int i = 0; i < colors.length() - 1; i++){ \
+       ColorStop currentColor = colors[i]; \
+       bool isInBetween = currentColor.position <= factor; \
+       index = int(mix(float(index), float(i), float(isInBetween))); \
+    } \
+    ColorStop currentColor = colors[index]; \
+    ColorStop nextColor = colors[index + 1]; \
+    float range = nextColor.position - currentColor.position; \
+    float lerpFactor = (factor - currentColor.position) / range; \
+    finalColor = mix(currentColor.color, nextColor.color, lerpFactor); \
+} \
+
+----------------------------
+
+Version 2 ( this version was used in this video: https://youtu.be/Ydu4epKEM3I ):
+
 /* ** ColorStop Point data structure for the COLOR_RAMP macro 
 color -> the color of the color stop
 position -> position of the color stop on the color ramp -> [0, 1]
@@ -20,6 +61,45 @@ vec3 finalColor -> the final color based on the factor
        ColorStop currentColor = colors[i]; \
        bool isInBetween = currentColor.position <= factor; \
        index = isInBetween ? i : index; \
+    } \
+    ColorStop currentColor = colors[index]; \
+    ColorStop nextColor = colors[index + 1]; \
+    float range = nextColor.position - currentColor.position; \
+    float lerpFactor = (factor - currentColor.position) / range; \
+    finalColor = mix(currentColor.color, nextColor.color, lerpFactor); \
+} \
+
+----------------------------
+
+Version 3 ( possibly the fastest version, using a mix instead of a ternary operator ):
+
+/* ** ColorStop Point data structure for the COLOR_RAMP macro 
+color -> the color of the color stop
+position -> position of the color stop on the color ramp -> [0, 1]
+*/
+struct ColorStop {
+    vec3 color;
+    float position;
+};
+
+/* ** COLOR_RAMP macro -> based on Blender's ColorRamp Node in the shading tab
+ColorStop[?] colors -> array of color stops that can have any length
+float factor -> the position that you want to know the color of -> [0, 1]
+vec3 finalColor -> the final color based on the factor 
+
+Line 5 Of The Macro:
+// possibly is bad for performance 
+index = isInBetween ? i : index; \
+
+Taken From: https://stackoverflow.com/a/26219603/19561482 
+index = int(mix(float(index), float(i), float(isInBetween))); \
+*/
+#define COLOR_RAMP(colors, factor, finalColor) { \
+    int index = 0; \
+    for(int i = 0; i < colors.length() - 1; i++){ \
+       ColorStop currentColor = colors[i]; \
+       bool isInBetween = currentColor.position <= factor; \
+       index = int(mix(float(index), float(i), float(isInBetween))); \
     } \
     ColorStop currentColor = colors[index]; \
     ColorStop nextColor = colors[index + 1]; \
